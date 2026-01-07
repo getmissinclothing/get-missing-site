d3448949f745a16433e7e081b0f715461766ecf8
getmissinclothing <getmissinbrandclothing@gmail.com>
2026-01-07 00:52:59 -0500

    Create page.js

---
+++ app/product/page.js
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Product() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>Get Missin™ “No Turning Back” Tee</h1>
+      <p>$44.00</p>
+      <button>Secure Checkout</button>
+    </main>
+  );
+}
